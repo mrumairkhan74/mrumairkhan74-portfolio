c6be32bf6d226063ccfa368c1430b3d43a9f4251
Umair Khan <mrumairkhan74@gmail.com>
2025-06-03 15:34:44 +0500

    setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./components/pages/Home";
+
 import Navbar from "./components/pages/Navbar";
 import { useState, useEffect } from "react";
 import Loading from "./components/background/Loading";
 import Footer from "./components/pages/Footer";
-
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Skills from "./components/pages/Skills";
+import Projects from "./components/Project/Projects";
+import Contact from "./components/pages/Contact";
 function App() {
   const [loading, setLoading] = useState(true);
 
@@ -20,6 +24,10 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
       <Footer />
     </BrowserRouter>
